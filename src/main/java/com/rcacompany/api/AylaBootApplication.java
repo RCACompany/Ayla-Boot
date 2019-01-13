c70e63237eefc81d730beed079022c96bad2a58b
RCA <cger.rcafriburgo@gmail.com>
2019-01-13 17:46:23 -0200

    Versão inicial do projeto.

---
+++ src/main/java/com/rcacompany/api/AylaBootApplication.java
@@ -0,0 +1,14 @@
+package com.rcacompany.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AylaBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AylaBootApplication.class, args);
+	}
+
+}
+
